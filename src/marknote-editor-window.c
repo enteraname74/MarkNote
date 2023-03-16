159e520019deab009581ac8e53569babaf4f3a2e
enteraname74 <noahpenin@gmail.com>
2023-03-16 22:06:08 +0100

    Started working on editor screen

--- src/marknote-editor-window.c
+++ src/marknote-editor-window.c
@@ -19,46 +19,27 @@
  */
 
 #include "config.h"
-
-#include "marknote-window.h"
 #include "marknote-editor-window.h"
 
-struct _MarknoteWindow
-{
+struct _MarknoteEditorWindow {
   AdwApplicationWindow  parent_instance;
-
-  /* Template widgets */
-  GtkHeaderBar        *header_bar;
   GtkLabel            *label;
-  GtkButton           *new_windows_button;
-};
-
-G_DEFINE_FINAL_TYPE (MarknoteWindow, marknote_window, ADW_TYPE_APPLICATION_WINDOW)
-
-static void click_button(GtkWidget *widget, gpointer data){
-  MarknoteWindow *current_window = (MarknoteWindow *)data;
-  GtkWindow *window;
-
-  window = g_object_new (MARKNOTE_TYPE_EDITOR_WINDOW,
-                           "application", current_window->parent_instance,
-                           NULL);
-
-  gtk_window_present (window);
+  GtkButton           *back_to_menu_button;
 }
 
-static void marknote_window_class_init (MarknoteWindowClass *klass)
+G_DEFINE_FINAL_TYPE(EditorWindow, marknote_editor_window, ADW_TYPE_APPLICATION_WINDOW)
+
+static void marknote_editor_window_class_init (MarknoteEditorWindowClass *klass)
 {
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
-  gtk_widget_class_set_template_from_resource (widget_class, "/com/github/MarkNote/ui/marknote-main-window.ui");
+  gtk_widget_class_set_template_from_resource (widget_class, "/com/github/MarkNote/ui/marknote-editor-window.ui");
   gtk_widget_class_bind_template_child (widget_class, MarknoteWindow, header_bar);
   gtk_widget_class_bind_template_child (widget_class, MarknoteWindow, label);
-  gtk_widget_class_bind_template_child(widget_class, MarknoteWindow, new_windows_button);
+  gtk_widget_class_bind_template_child(widget_class, MarknoteWindow, back_to_menu_button);
 }
 
-static void marknote_window_init (MarknoteWindow *self)
+static void marknote_editor_window_init (MarknoteEditorWindow *self)
 {
   gtk_widget_init_template (GTK_WIDGET (self));
-
-  g_signal_connect (GTK_BUTTON(self->new_windows_button), "clicked", G_CALLBACK (click_button), (gpointer)self);
 }
