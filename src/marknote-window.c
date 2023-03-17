5b5e28fcfeeecf11e1be580cfd1b259d96f3db83
enteraname74 <noahpenin@gmail.com>
2023-03-17 23:31:22 +0100

    Can now save file.

--- src/marknote-window.c
+++ src/marknote-window.c
@@ -43,12 +43,12 @@ static void add_tab(MarknoteWindow *window, GtkWidget *text_view, char *file_nam
   gtk_widget_set_vexpand (GTK_WIDGET(scrolled_window), true);
   gtk_widget_set_hexpand(GTK_WIDGET(scrolled_window), true);
 
-  //adw_tab_view_append(ADW_TAB_VIEW (window->tab_view), GTK_WIDGET(scrolled_window));
   AdwTabPage *tab_page = adw_tab_view_add_page (ADW_TAB_VIEW (window->tab_view), GTK_WIDGET(scrolled_window), NULL);
   adw_tab_page_set_title (ADW_TAB_PAGE(tab_page), file_name);
 }
 
-static void on_open_response(GtkNativeDialog *native, int response, gpointer data) {
+static void on_open_response(GtkNativeDialog *native, int response, gpointer data)
+{
   char *file_name;
   char *contents;
   gsize length;
@@ -77,9 +77,100 @@ static void on_open_response(GtkNativeDialog *native, int response, gpointer dat
       g_free(file_name);
     }
 
-  gtk_window_destroy (GTK_WINDOW (native));
+  g_object_unref(native);
 }
 
+static void save_file_complete (GObject *source_object,
+                                GAsyncResult *result,
+                                gpointer user_data)
+{
+  GFile *file = G_FILE (source_object);
+
+  g_autoptr (GError) error = NULL;
+  g_file_replace_contents_finish (file, result, NULL, &error);
+
+  g_autofree char *display_name = NULL;
+  g_autoptr (GFileInfo) info = g_file_query_info (file,
+                                                  "standard::display-name",
+                                                  G_FILE_QUERY_INFO_NONE,
+                                                  NULL,
+                                                  NULL);
+  if (info != NULL)
+    {
+      display_name = g_strdup(g_file_info_get_attribute_string (info, "standard::display-name"));
+    }
+  else
+    {
+      display_name = g_file_get_basename (file);
+    }
+
+  if (error != NULL) {
+    g_printerr("Unable to save “%s”: %s\n",
+               display_name,
+               error->message);
+  }
+}
+
+static void save_file(MarknoteWindow *self, GFile *file)
+{
+  AdwTabPage *current_page = adw_tab_view_get_selected_page (ADW_TAB_VIEW(self->tab_view));
+  GtkWidget *page_child = adw_tab_page_get_child (ADW_TAB_PAGE(current_page));
+  GtkWidget *text_view = gtk_widget_get_first_child (GTK_WIDGET(page_child));
+
+  GtkTextBuffer *buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW(text_view));
+
+  // Retrieve the iterator at the start of the buffer
+  GtkTextIter start;
+  gtk_text_buffer_get_start_iter (buffer, &start);
+
+  // Retrieve the iterator at the end of the buffer
+  GtkTextIter end;
+  gtk_text_buffer_get_end_iter (buffer, &end);
+
+  // Retrieve all the visible text between the two bounds
+  char *text = gtk_text_buffer_get_text (buffer, &start, &end, FALSE);
+
+  // If there is nothing to save, return early
+  if (text == NULL)
+   return;
+
+  g_autoptr(GBytes) bytes = g_bytes_new_take (text, strlen (text));
+
+  // Start the asynchronous operation to save the data into the file
+  g_file_replace_contents_bytes_async (file,
+                                      bytes,
+                                      NULL,
+                                      FALSE,
+                                      G_FILE_CREATE_NONE,
+                                      NULL,
+                                      save_file_complete,
+                                      self);
+}
+
+static void on_save_response(GtkNativeDialog *native, int response, MarknoteWindow *self)
+{
+  if (response == GTK_RESPONSE_ACCEPT)
+    {
+      g_autoptr (GFile) file = gtk_file_chooser_get_file (GTK_FILE_CHOOSER (native));
+
+      save_file(self, file);
+    }
+
+  g_object_unref(native);
+}
+
+static void save_as_file(GAction *action G_GNUC_UNUSED,
+                         GVariant *param G_GNUC_UNUSED,
+                         MarknoteWindow *self)
+{
+  GtkFileChooserNative *native = gtk_file_chooser_native_new ("Save File As",
+                                                              GTK_WINDOW(self),
+                                                              GTK_FILE_CHOOSER_ACTION_SAVE,
+                                                              "_Save",
+                                                              "_Cancel");
+  g_signal_connect (native, "response", G_CALLBACK(on_save_response), self);
+  gtk_native_dialog_show(GTK_NATIVE_DIALOG(native));
+}
 
 static void open_file_chooser(GtkWidget *widget, gpointer data) {
   MarknoteWindow *current_window = (MarknoteWindow *)data;
@@ -112,6 +203,11 @@ static void marknote_window_class_init (MarknoteWindowClass *klass)
 static void marknote_window_init (MarknoteWindow *self)
 {
   gtk_widget_init_template (GTK_WIDGET (self));
+
+  g_autoptr (GSimpleAction) save_action = g_simple_action_new ("save-as", NULL);
+  g_signal_connect (save_action, "activate", G_CALLBACK (save_as_file), self);
+  g_action_map_add_action (G_ACTION_MAP (self), G_ACTION (save_action));
+
 
   g_signal_connect (GTK_BUTTON(self->open_file), "clicked", G_CALLBACK (open_file_chooser), (gpointer)self);
 
